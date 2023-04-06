d2177784eff5a378fca5dfed8cd6c12f1956ce45
oeyXD <joeysantos2007@gmail.com>
2023-04-06 18:24:47 +0800

    Edited to show the designated directions per line

--- src/Plot.js
+++ src/Plot.js
@@ -1,17 +1,13 @@
 import downloadKmlFile from './create-kml-file'; import React, { useState } from 'react'
 let num = 1;
-
-
-
 //<input type="text" value={Dir1st} onChange={(event) => onValUpdate(index, event)}
 //                    name="Dir1st" className="form-control" />
-
 function TableRows({ rows, tableRowRemove, onValUpdate }) {
     return rows.map((rowsData, index) => {
         const { Dir1st, Deg, Min, Dir2nd, Dist } = rowsData;
         return (
             <tr key={index}>
-                <td>Point {count} to {count + 1}</td>
+                <td>Point {count.length} to {count.length + 1}</td>
                 <td class="Dir1stclass">
                     <select class={"Dir1st" + count} >
                         <option value={"card1"} selected={true} disabled="disabled" >N or S</option>
@@ -38,34 +34,43 @@ function TableRows({ rows, tableRowRemove, onValUpdate }) {
                 <td><button className="btn btn-dark" onClick={() => tableRowRemove(index)} >
                     Delete Row </button>
                 </td>
-
             </tr>
-
         );
     });
 }
-export let count = 0; console.log(count); let origLat, origLong, fileName;
+export const count = []; let extraCount = 1; console.log(count); let origLat, origLong, fileName, origDeg, origMin, origDist;
 function Plot() {
     const [rows, initRow] = useState([]);
     const addRowTable = () => {
         const data = { Dir1st: "", Deg: "", Min: "", Dir2nd: "", Dist: "" };
-        initRow([...rows, data]); count += 1;
+        initRow([...rows, data]); count.push(extraCount); console.log("Here is the length of count: " + count.length);
+        updateNos()
     };
     const tableRowRemove = (index) => {
         const dataRow = [...rows];
         dataRow.splice(index, 1); initRow(dataRow);
-        count -= 1;
-
+        extraCount -= 1; count.pop(); console.log("Here is the length of count: ", count.length)
+        updateNos()
     };
     const onValUpdate = (i, event) => {
         const { name, value } = event.target; const data = [...rows]; data[i][name] = value; initRow(data);
     };
 
+    function updateNos() {
+        let i = 7; let Nos = [];
+        while (i < (count.length + 6)) {
+            console.log("The value of i is : " + i, "\n The value of count.length is :", count.length)
+            document.querySelector("#root > div:nth-child(3) > table > tbody > tr:nth-child(" + i + ") > td:nth-child(1)").innerHTML = "Point " + Number(i - 5) + " to " + Number(i - 4);
+            //document.querySelector(Nos[i + 7]).innerHTML = "Point " + Nos[1][Nos[1].length - 20] + " to " + i + 1;
+            i += 1
+        }
+    }
+
     return (
         <div>
-            <p>Word of advice: Last Point connects to the Point 1, which is the initial coordinates you've entered.</p>
-            <input type="number" id="origLatId" value={origLat} name="origLat" placeholder="Enter Latitude" />
-            <input type="number" id="origLongId" value={origLong} name="origLong" placeholder="Enter Longitude" />
+            <input type="number" id="origLatId" value={origLat} name="origLat" placeholder="Enter BLLM Latitude" />
+            <input type="number" id="origLongId" value={origLong} name="origLong" placeholder="Enter BLLM Longitude" />
+            <br />
             <table className="table table-striped">
                 <thead><tr>
                     <th>No.</th><th>Dir1</th><th>Deg</th><th>Min</th><th>Dir2</th><th>Distance (meters, m, mtrs.)</th>
@@ -73,12 +78,34 @@ function Plot() {
                     </th>
                 </tr></thead>
                 <tbody>
-
+                    <td>Point 1 to 2</td>
+                    <td class="Dir1stIniclass">
+                        <select class="Dir1stIni" >
+                            <option value={"card1"} selected={true} disabled="disabled" >N or S</option>
+                            <option value={"north"}>North (N)</option>
+                            <option value={"south"}>South (S)</option>
+                        </select>
+                    </td>
+                    <td>
+                        <input type="number" id='origDegId' value={origDeg} name="origDeg" placeholder='Enter Initial Degree(s)'></input>
+                    </td>
+                    <td>
+                        <input type="number" id='origMinId' value={origMin} name="origMin" placeholder='Enter Initial Minute(s)'></input>
+                    </td>
+                    <td class="Dir2ndIniclass">
+                        <select class="Dir2ndIni" >
+                            <option value={"card2"} selected={true} disabled="disabled">E or W</option>
+                            <option value={"east"}>East (E)</option>
+                            <option value={"west"}>West (W)</option>
+                        </select>
+                    </td>
+                    <td>
+                        <input type="number" id='origDistId' value={origDist} name="origDist" placeholder='Enter Initial Distance'></input>m
+                    </td>
                     <TableRows rows={rows} tableRowRemove={tableRowRemove} onValUpdate={onValUpdate}
                     />
                 </tbody>
             </table>
-
             <input id="fileNameId" value={fileName} name="fileName" placeholder="Enter File Name" />
             <button primary onClick={downloadKmlFile} >Download</button>
         </div>
